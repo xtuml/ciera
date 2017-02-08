a600ff66bdba5aeb8dd7c5fbe4a34c84990cf905
Levi Starrett <levi@roxsoftware.com>
2017-02-08 16:57:25 -0500

    added validation to ciera

---
+++ model/ciera/gen/LOG_bridge.c
@@ -0,0 +1,82 @@
+/*----------------------------------------------------------------------------
+ * File:  LOG_bridge.c
+ *
+ * Description:
+ * Methods for bridging to an external entity.
+ *
+ * External Entity:  Logging (LOG)
+ * 
+ *--------------------------------------------------------------------------*/
+
+#include "ciera_sys_types.h"
+#include "LOG_bridge.h"
+
+/*
+ * Bridge:  LogSuccess
+ */
+void
+LOG_LogSuccess( c_t * p_message )
+{
+  fprintf( stderr, "%s\n", p_message );
+}
+
+
+/*
+ * Bridge:  LogFailure
+ */
+void
+LOG_LogFailure( c_t * p_message )
+{
+  fprintf( stderr, "%s\n", p_message );
+}
+
+
+/*
+ * Bridge:  LogInfo
+ */
+void
+LOG_LogInfo( c_t * p_message )
+{
+  fprintf( stderr, "%s\n", p_message );
+}
+
+
+/*
+ * Bridge:  LogDate
+ */
+void
+LOG_LogDate( Escher_Date_t p_d, c_t * p_message )
+{
+  /* Replace/Insert your implementation code here... */
+}
+
+
+/*
+ * Bridge:  LogTime
+ */
+void
+LOG_LogTime( c_t * p_message, Escher_TimeStamp_t p_t )
+{
+  /* Replace/Insert your implementation code here... */
+}
+
+
+/*
+ * Bridge:  LogReal
+ */
+void
+LOG_LogReal( c_t * p_message, const r_t p_r )
+{
+  fprintf( stderr, "%s, %f\n", p_message, p_r );
+}
+
+
+/*
+ * Bridge:  LogInteger
+ */
+void
+LOG_LogInteger( const i_t p_message )
+{
+  fprintf( stderr, "%d\n", p_message );
+}
+
