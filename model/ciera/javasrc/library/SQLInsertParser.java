c4d49040cea8b609c86fa4771e80c43d3d453561
Levi Starrett <levi@roxsoftware.com>
2017-03-03 12:47:11 -0500

    set up dependency on sqlinsert project

--- model/ciera/javasrc/library/SQLInsertParser.java
+++ model/ciera/javasrc/library/SQLInsertParser.java
@@ -1,10 +1,14 @@
 package library;
 
+import java.io.*;
 import java.util.ArrayList;
+
 import lib.LOG;
 import org.xtuml.bp.core.ComponentInstance_c;
+import sqlinsert.SqlInsertHandler;
+import sqlinsert.SqlInsertParse;
 
-public class SQLInsertParser implements IparseToProvider, ISQLFromProvider {
+public class SQLInsertParser implements IparseToProvider, ISQLFromProvider, SqlInsertHandler {
 
     private ISQLToProvider SQL;
     private IparseFromProvider parse;
@@ -25,12 +29,27 @@ public class SQLInsertParser implements IparseToProvider, ISQLFromProvider {
     	}
     }
     
+	@Override
+	public void insert(String table, ArrayList<String> values) {
+		if ( table != null && !table.equals("") && values != null && values.size() == ISQLToProvider.LEN_VALUES ) {
+			SQL.insert( null, table, values );
+		}
+		else {
+    		LOG.LogFailure("SQLInserParser: Malformed insert statement.");
+		}
+	}
+    
     private void parse() {
-        ArrayList<String> values = new ArrayList<String>();
-        values.add("courtney");
-        values.add("is");
-        values.add("hot");
-        SQL.insert( null, "levi", values );
+        SqlInsertParse sqlinsert = new SqlInsertParse();
+        File infile = new File("/Users/levistarrett/git/xtuml/ciera/model/ciera/test_data/ooaofgraphics.sql");
+        InputStream in = null;
+        try {
+        	in = new FileInputStream( infile );
+        }
+        catch ( FileNotFoundException e ) {
+        	System.err.println(e);
+        }
+        sqlinsert.parse( in, this );
         done();
     }
     
