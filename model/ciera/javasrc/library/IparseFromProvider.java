030d01caf709c169ae7a118bc999d01203ba562f
Levi Starrett <levi@roxsoftware.com>
2017-03-03 12:28:34 -0500

    removed unnecessary white space

--- model/ciera/javasrc/library/IparseFromProvider.java
+++ model/ciera/javasrc/library/IparseFromProvider.java
@@ -3,7 +3,5 @@ package library;
 import org.xtuml.bp.core.ComponentInstance_c;
 
 public interface IparseFromProvider {
-
     public void done(ComponentInstance_c senderReceiver);
-
 }
--- model/ciera/javasrc/library/IparseToProvider.java
+++ model/ciera/javasrc/library/IparseToProvider.java
@@ -3,7 +3,5 @@ package library;
 import org.xtuml.bp.core.ComponentInstance_c;
 
 public interface IparseToProvider {
-
     public void parse(ComponentInstance_c senderReceiver);
-
 }
