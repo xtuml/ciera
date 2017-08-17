a423e4c0fd90407a40f5e294acb04d9a7da26411
Levi Starrett <levi@roxsoftware.com>
2017-08-17 15:52:27 -0400

    added initialization for relationship pointers

--- ciera/src/java/t.associatedclassref.java
+++ ciera/src/java/t.associatedclassref.java
@@ -1,6 +1,17 @@
+if ( declaration ) {
 T_b("    ");
 T_b(target_type_name);
 T_b(" ");
 T_b(ref_name);
 T_b(";");
 T_b("\n");
+} else if ( initialize ) {
+T_b("        ");
+T_b(ref_name);
+T_b(" = ");
+T_b(target_type_name);
+T_b(".empty");
+T_b(target_type_name);
+T_b(";");
+T_b("\n");
+}
--- ciera/templates/t.associatedclassref.java
+++ ciera/templates/t.associatedclassref.java
@@ -1,1 +1,5 @@
+.if ( declaration )
     ${target_type_name} ${ref_name};
+.elif ( initialize )
+        ${ref_name} = ${target_type_name}.empty${target_type_name};
+.end if
