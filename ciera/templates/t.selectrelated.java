bceda23eb6bd8ba81d8d6a969b24696189e9754a
Levi Starrett <levi@roxsoftware.com>
2017-08-27 13:07:27 -0400

    all association selections are working

--- ciera/templates/t.selectrelated.java
+++ ciera/templates/t.selectrelated.java
@@ -1,21 +1,21 @@
 .if ( isset )
   .if ( isempty )
     @Override
-    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}( Where condition ) throws XtumlException {
+    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}${phrase}( Where condition ) throws XtumlException {
         return ${target_type_name}.empty${target_type_name};
     }
   .else
-    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}() throws XtumlException {
-        return selectMany${target_key_letters}sOnR${rel_numb}( null );
+    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}${phrase}() throws XtumlException {
+        return selectMany${target_key_letters}sOnR${rel_numb}${phrase}( null );
     }
 
-    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}( Where condition ) throws XtumlException {
+    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}${phrase}( Where condition ) throws XtumlException {
         ${target_type_name} return_set = new ${target_type_name}();
         for ( ModelInstance selected : this ) {
     .if ( is_many )
-            return_set.addAll( ((${class_name})selected).selectMany${target_key_letters}sOnR${rel_numb}( condition ) );
+            return_set.addAll( ((${class_name})selected).selectMany${target_key_letters}sOnR${rel_numb}${phrase}( condition ) );
     .else
-            return_set.add( ((${class_name})selected).selectOne${target_key_letters}OnR${rel_numb}( condition ) );
+            return_set.add( ((${class_name})selected).selectOne${target_key_letters}OnR${rel_numb}${phrase}( condition ) );
     .end if
         }
         if ( return_set.isEmpty() ) return ${target_type_name}.empty${target_type_name};
@@ -26,27 +26,27 @@
   .if ( isempty )
     .if ( is_many )
     @Override
-    public ${target_type_obj_name} selectAny${target_key_letters}OnR${rel_numb}( Where condition ) throws XtumlException {
+    public ${target_type_obj_name} selectAny${target_key_letters}OnR${rel_numb}${phrase}( Where condition ) throws XtumlException {
         return ${target_type_obj_name}.empty${target_type_obj_name};
     }
 
     @Override
-    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}( Where condition ) throws XtumlException {
+    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}${phrase}( Where condition ) throws XtumlException {
         return ${target_type_name}.empty${target_type_name};
     }
     .else
     @Override
-    public ${target_type_name} selectOne${target_key_letters}OnR${rel_numb}( Where condition ) throws XtumlException {
+    public ${target_type_name} selectOne${target_key_letters}OnR${rel_numb}${phrase}( Where condition ) throws XtumlException {
         return ${target_type_name}.empty${target_type_name};
     }
     .end if
   .else
     .if ( is_many )
-    public ${target_type_obj_name} selectAny${target_key_letters}OnR${rel_numb}() throws XtumlException {
-        return selectAny${target_key_letters}OnR${rel_numb}( null );
+    public ${target_type_obj_name} selectAny${target_key_letters}OnR${rel_numb}${phrase}() throws XtumlException {
+        return selectAny${target_key_letters}OnR${rel_numb}${phrase}( null );
     }
 
-    public ${target_type_obj_name} selectAny${target_key_letters}OnR${rel_numb}( Where condition ) throws XtumlException {
+    public ${target_type_obj_name} selectAny${target_key_letters}OnR${rel_numb}${phrase}( Where condition ) throws XtumlException {
         checkLiving();
       .if ( is_unconditional )
         if ( ${ref_name} instanceof EmptyInstanceSet ) throw new ModelIntegrityException( "Uncoditional association with no related instance." );
@@ -56,11 +56,11 @@
       .end if
     }
 
-    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}() throws XtumlException {
-        return selectMany${target_key_letters}sOnR${rel_numb}( null );
+    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}${phrase}() throws XtumlException {
+        return selectMany${target_key_letters}sOnR${rel_numb}${phrase}( null );
     }
 
-    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}( Where condition ) throws XtumlException {
+    public ${target_type_name} selectMany${target_key_letters}sOnR${rel_numb}${phrase}( Where condition ) throws XtumlException {
         checkLiving();
       .if ( is_unconditional )
         if ( ${ref_name} instanceof EmptyInstanceSet ) throw new ModelIntegrityException( "Uncoditional association with no related instance." );
@@ -70,11 +70,11 @@
       .end if
     }
     .else
-    public ${target_type_name} selectOne${target_key_letters}OnR${rel_numb}() throws XtumlException {
-        return selectOne${target_key_letters}OnR${rel_numb}( null );
+    public ${target_type_name} selectOne${target_key_letters}OnR${rel_numb}${phrase}() throws XtumlException {
+        return selectOne${target_key_letters}OnR${rel_numb}${phrase}( null );
     }
     
-    public ${target_type_name} selectOne${target_key_letters}OnR${rel_numb}( Where condition ) throws XtumlException {
+    public ${target_type_name} selectOne${target_key_letters}OnR${rel_numb}${phrase}( Where condition ) throws XtumlException {
         checkLiving();
       .if ( is_unconditional )
         if ( ${ref_name} instanceof EmptyInstance ) throw new ModelIntegrityException( "Uncoditional association with no related instance." );
