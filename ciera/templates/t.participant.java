ebf9d5a2b663909908496aabec41c57a1d8fe965
Levi Starrett <levi@roxsoftware.com>
2017-08-29 21:01:23 -0400

    added relate methods

--- ciera/templates/t.participant.java
+++ ciera/templates/t.participant.java
@@ -1,20 +1,20 @@
 .if ( is_many )
-    public void add${capital_ref_name}OnR${rel_numb}( ${target_type_name} ${inst_name} ) throws XtumlException {
+    public void add${capital_ref_name}( ${target_type_name} ${inst_name} ) throws XtumlException {
         if ( !(${ref_name}.contains( ${inst_name} )) ) ${ref_name}.add( ${inst_name} );
         else throw new LinkException( "Cannot link to already linked relationship." );
     }
 
-    public void remove${capital_ref_name}OnR${rel_numb}( ${target_type_name} ${inst_name} ) throws XtumlException {
+    public void remove${capital_ref_name}( ${target_type_name} ${inst_name} ) throws XtumlException {
         if ( ${ref_name}.contains( ${inst_name} ) ) ${ref_name}.remove( ${inst_name} );
         else throw new LinkException( "Cannot unlink non-linked relationship." );
     }
 .else
-    public void set${capital_ref_name}OnR${rel_numb}( ${target_type_name} ${inst_name} ) throws XtumlException {
+    public void set${capital_ref_name}( ${target_type_name} ${inst_name} ) throws XtumlException {
         if ( ${ref_name} instanceof EmptyInstance ) ${ref_name} = ${inst_name};
         else throw new LinkException( "Cannot link to already linked relationship." );
     }
 
-    public void clear${capital_ref_name}OnR${rel_numb}() throws XtumlException {
+    public void clear${capital_ref_name}() throws XtumlException {
         if ( !(${ref_name} instanceof EmptyInstance) ) ${ref_name} = ${target_type_name}.empty${target_type_name};
         else throw new LinkException( "Cannot unlink non-linked relationship." );
     }
---
+++ ciera/templates/t.relate.java
@@ -0,0 +1,32 @@
+.if ( is_formalizer )
+    public synchronized void relateTo${target_key_letters}AcrossR${rel_numb}${phrase}( ${target_type_name} ${target_inst_name} ) throws XtumlException {
+        checkLiving();
+        ${target_inst_name}.checkLiving();
+  .if ( is_many )
+        if ( !(${ref_name}.contains( ${target_inst_name} )) ) {
+            ${ref_name}.add( ${target_inst_name} );
+    .if ( corresponding_is_many )
+            ${target_inst_name}.add${capital_ref_name}( this );
+    .else
+            ${target_inst_name}.set${capital_ref_name}( this );
+    .end if
+            // TODO set referential attributes
+        }
+  .else
+        if ( ${ref_name} instanceof EmptyInstance ) {
+            ${ref_name} = ${target_inst_name};
+    .if ( corresponding_is_many )
+            ${target_inst_name}.add${capital_ref_name}( this );
+    .else
+            ${target_inst_name}.set${capital_ref_name}( this );
+    .end if
+            // TODO set referential attributes
+        }
+  .end if
+        else throw new LinkException( "Cannot link to already linked relationship." );
+    }
+.else
+    public synchronized void relateTo${target_key_letters}AcrossR${rel_numb}${phrase}( ${target_type_name} ${target_inst_name} ) throws XtumlException {
+        ${target_inst_name}.relateTo${class_kl}AcrossR${rel_numb}${corresponding_phrase}( this );
+    }
+.end if
