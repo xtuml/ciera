12d4e3863be574ec5d9785ba69048765004a6645
Levi Starrett <levi@roxsoftware.com>
2017-09-20 21:23:43 -0400

    deleted ciera_test project

--- ciera_test/gen/postprocess.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while read f; do
-  stat "../src-gen/${f:7}" &> /dev/null
-  if [[ 0 == $? ]]; then
-    mv "../src-gen/${f:7}" "../src-gen/${f:7}.orig"
-  fi
-done <<< $(find ../src -name "*\.java" -print0)
