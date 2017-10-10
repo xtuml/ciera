4ee7edd8c946ddf43aa1df0c59cdd64f72828105
Levi Starrett <levi@roxsoftware.com>
2017-10-10 10:25:54 -0400

    updated run script

--- util/id_check/run_id_check.sh
+++ util/id_check/run_id_check.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+GEN_ERATE=~/xtuml/$BPVER/BridgePoint.app/Contents/Eclipse/tools/mc/bin/gen_erate.pyz
+OOA_SCHEMA=~/xtuml/$BPVER/BridgePoint.app/Contents/Eclipse/tools/mc/schema/sql/xtumlmc_schema.sql
 if [[ $# == 1 ]]; then
-  pypy ~/bin/gen_erate -nopersist -import ~/git/mc/schema/sql/xtumlmc_schema.sql \
+  pypy $GEN_ERATE -nopersist -import $OOA_SCHEMA \
                                   -import id_check_model.sql \
                                   -import $1 \
                                   -arch id_check.arc \
                                   -arch model_check.arc
+elif [[ $# == 2 ]]; then
+  if [[ $1 == "id_check" ]]; then
+    pypy $GEN_ERATE -nopersist -import $OOA_SCHEMA \
+                                    -import id_check_model.sql \
+                                    -import $2 \
+                                    -arch id_check.arc
+  elif [[ $1 == "model_check" ]]; then
+    pypy $GEN_ERATE -nopersist -import $OOA_SCHEMA \
+                                    -import id_check_model.sql \
+                                    -import $2 \
+                                    -arch model_check.arc
+  fi
+else
+  echo "Usage: $0 [id_check|model_check] <model file>"
+  echo "       $0 <model file>"
 fi
