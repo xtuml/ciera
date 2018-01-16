9bb76acaf96daab330951f21b18d0fc028c58911
Levi Starrett <levi@roxsoftware.com>
2018-01-16 09:27:09 -0500

    working on selectors still

--- ciera/src/java/t.emptyselector.java
+++ ciera/src/java/t.emptyselector.java
@@ -1,3 +1,23 @@
+if ( is_many ) {
+T_b("    ");
+T_b("@Override");
+T_b("\n");
+T_b("    ");
+T_b("public ");
+T_b(type_name);
+T_b(" ");
+T_b(name);
+T_b("( IWhere condition ) {");
+T_b("\n");
+T_b("        ");
+T_b("return new ");
+T_b(type_name);
+T_b("();");
+T_b("\n");
+T_b("    ");
+T_b("}");
+T_b("\n");
+} else {
 T_b("    ");
 T_b("@Override");
 T_b("\n");
@@ -18,3 +38,4 @@ T_b("\n");
 T_b("    ");
 T_b("}");
 T_b("\n");
+}
--- ciera/templates/t.emptyselector.java
+++ ciera/templates/t.emptyselector.java
@@ -1,4 +1,11 @@
+.if ( is_many )
+    @Override
+    public ${type_name} ${name}( IWhere condition ) {
+        return new ${type_name}();
+    }
+.else
     @Override
     public ${type_name} ${name}( IWhere condition ) {
         return ${type_name}.empty${type_name};
     }
+.end if
