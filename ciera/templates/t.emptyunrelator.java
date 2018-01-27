97d2fa4536b49e642fc4e7d74e388e1c37d65c4e
Levi Starrett <levi@roxsoftware.com>
2018-01-26 21:26:50 -0500

    reorganized templates into subsystem folders

--- ciera/templates/t.emptyunrelator.java
+++
@@ -1,4 +0,0 @@
-    @Override
-    public void unrelateFrom${type_key_letters}AcrossR${rel_numb}${phrase}( ${type_name} $l{type_name} ) throws XtumlException {
-        throw new EmptyInstanceException( "Unrelate of empty instance" );
-    }
