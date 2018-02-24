ffdbc71f825754c5314a28a9016e08531f545502
Levi Starrett <levi@roxsoftware.com>
2018-02-24 15:00:05 -0500

    removed state machine java files

--- summit/src/io/ciera/summit/statemachine/EventTarget.java
+++
@@ -1,9 +0,0 @@
-package io.ciera.summit.statemachine;
-
-import io.ciera.summit.exceptions.XtumlException;
-
-public interface EventTarget {
-    
-    public void transition( Event e ) throws XtumlException;
-
-}
