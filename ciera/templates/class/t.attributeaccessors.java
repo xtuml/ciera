d6d5f50bd66beba85722b91e2109c4db6c2f83d6
Levi Starrett <levi@roxsoftware.com>
2018-02-25 13:58:23 -0500

    throw error if empty instance for attribute access

--- ciera/templates/class/t.attributeaccessors.java
+++ ciera/templates/class/t.attributeaccessors.java
@@ -1,5 +1,6 @@
     public ${type_name} get${capital_name}() throws XtumlException {
         checkLiving();
+        if ( this instanceof IEmptyInstance ) throw new EmptyInstanceException( "Cannot get attribute of empty instance." );
 ${referential_getters}
 ${attribute_processor}
         return ${name};
@@ -7,6 +8,7 @@ ${attribute_processor}
 
     public void set${capital_name}( ${type_name} ${name} ) throws XtumlException {
         checkLiving();
+        if ( this instanceof IEmptyInstance ) throw new EmptyInstanceException( "Cannot set attribute of empty instance." );
 ${referential_setters}
         this.${name} = ${name};
     }
