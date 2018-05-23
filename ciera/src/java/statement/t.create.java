cf6ef4d0efb5d5e6bf10b34b81e8b252c1407701
Levi Starrett <levi@roxsoftware.com>
2018-05-23 13:30:44 -0400

    implemented instance creates

--- ciera/src/java/statement/t.create.java
+++
@@ -1,8 +0,0 @@
-T_b(indent);
-T_b(created_var_type);
-T_b(" ");
-T_b(created_var_name);
-T_b(" = population().createObjectInstance( ");
-T_b(created_var_type);
-T_b(" );");
-T_b("\n");
--- ciera/templates/statement/t.create.java
+++
@@ -1,1 +0,0 @@
-${indent}${created_var_type} ${created_var_name} = population().createObjectInstance( ${created_var_type} );
