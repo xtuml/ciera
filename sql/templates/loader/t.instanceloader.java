39c3d4ea10415a6441301cf37fa98e86d99511f4
Levi Starrett <levi@roxsoftware.com>
2018-06-06 12:46:06 -0400

    first codegen for sql working

---
+++ sql/templates/loader/t.instanceloader.java
@@ -0,0 +1,11 @@
+package ${self.package};
+
+import io.ciera.sql.util.IInstanceLoader;
+import java.util.List;
+
+public class ${self.name} implements IInstanceLoader {
+
+    public void insert( String tableName, List<Object> values ) {}
+    public void finish() {}
+
+}
