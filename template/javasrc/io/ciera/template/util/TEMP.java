4fd3ef43e4009dfc9a91c02574a583c20c108d12
Levi Starrett <levi@roxsoftware.com>
2018-06-26 15:58:18 -0400

    some reorganization preparing for transforming substitution variables

--- template/javasrc/io/ciera/template/util/TEMP.java
+++ template/javasrc/io/ciera/template/util/TEMP.java
@@ -1,11 +1,29 @@
 package io.ciera.template.util;
 
 import io.ciera.summit.exceptions.XtumlException;
+import io.ciera.summit.types.IXtumlType;
 import io.ciera.summit.types.XtumlString;
 
 public interface TEMP {
     
     public void append( XtumlString s ) throws XtumlException;
+
+    default public void append( boolean b ) throws XtumlException {
+        append( new XtumlString( Boolean.toString( b ) ) );
+    }
+
+    default public void append( int i ) throws XtumlException {
+        append( new XtumlString( Integer.toString( i ) ) );
+    }
+
+    default public void append( double d ) throws XtumlException {
+        append( new XtumlString( Double.toString( d ) ) );
+    }
+
+    default public <T extends IXtumlType<T>>void append( T o ) throws XtumlException {
+        append( new XtumlString( o.toString() ) );
+    }
+
     public XtumlString body() throws XtumlException;
     public void clear() throws XtumlException;
     public void emit( XtumlString file ) throws XtumlException;
