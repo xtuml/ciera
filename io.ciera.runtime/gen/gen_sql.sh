34d625b0d2d3f6692bb961dd0155b025abc3d552
Levi Starrett <levi@roxsoftware.com>
2018-11-01 14:54:44 -0400

    build libraries into standalone jars

--- io.ciera.runtime/gen/gen_sql.sh
+++ io.ciera.runtime/gen/gen_sql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CP=/Users/levi/xtuml/m6150.2018-09-12-1306_10129-build/BridgePoint.app/Contents/Eclipse/plugins/antlr-4.7.1-complete.jar
-java -Xmx500M -cp "$CP" org.antlr.v4.Tool $PWD/../grammar/SQL.g4      -o $PWD/../src-gen/io/ciera/runtime/instanceloading/sql/parser -listener -no-visitor -encoding UTF-8
+ANTLR=/Users/levi/xtuml/m6150.2018-09-12-1306_10129-build/BridgePoint.app/Contents/Eclipse/plugins/antlr-4.7.1-complete.jar
+java -Xmx500M -jar $ANTLR $PWD/../grammar/SQL.g -o $PWD/../src-gen/io/ciera/runtime/instanceloading/sql/parser -listener -no-visitor -encoding UTF-8
