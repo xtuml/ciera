54a14d89bcadfc1ea1a6bd7f1a0cbca5dfb69a14
Levi Starrett <levi@roxsoftware.com>
2018-11-02 21:35:32 -0400

    job #10 commited antlr jar

--- src/io.ciera.tool.template/gen/gen_template.sh
+++ src/io.ciera.tool.template/gen/gen_template.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ANTLR=/Users/levi/xtuml/m6150.2018-09-12-1306_10129-build/BridgePoint.app/Contents/Eclipse/plugins/antlr-4.7.1-complete.jar
+ANTLR=../../../release/antlr-4.7.1-complete.jar
 java -Xmx500M -jar $ANTLR $PWD/../grammar/RSLLexer.g -o $PWD/../src-gen/io/ciera/tool/templateengine/parser -listener -no-visitor -encoding UTF-8
 java -Xmx500M -jar $ANTLR $PWD/../grammar/RSL.g      -o $PWD/../src-gen/io/ciera/tool/templateengine/parser -listener -no-visitor -encoding UTF-8
