0d091fb6d606e9fab98d1dae3ded86467f058e6a
Levi Starrett <levi@roxsoftware.com>
2018-11-23 10:06:26 -0500

    job #26 getting things set up with maven

--- src/io.ciera.runtime/gen/gen_sql.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ANTLR=../../../release/antlr-4.7.1-complete.jar
-java -Xmx500M -jar $ANTLR $PWD/../grammar/SQL.g -o $PWD/../src-gen/io/ciera/runtime/instanceloading/sql/parser -listener -no-visitor -encoding UTF-8
