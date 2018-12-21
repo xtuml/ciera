c61852982321f3ea8849ef40c4a47347f20bfe3a
Levi Starrett <levi@roxsoftware.com>
2018-12-21 09:21:47 -0500

    job #19 removed '_return_value' stuff

--- src/io.ciera.tool.dynamodb/templates/statement/t.return.java
+++ src/io.ciera.tool.dynamodb/templates/statement/t.return.java
@@ -5,10 +5,5 @@ ${prefix}\
 .if ( "" == ret_expression_body )
 ${block_suffix}${indent}return;
 .else
-  .if ( returns_var )
-${indent}_return_value = ${ret_expression_body};
-${block_suffix}${indent}return _return_value;
-  .else
 ${block_suffix}${indent}return ${ret_expression_body};
-  .end if
 .end if
