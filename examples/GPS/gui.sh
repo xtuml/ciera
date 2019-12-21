0e8e21740baa81f778f05b011f1ac9d19a6c55fb
Levi Starrett <levi@roxsoftware.com>
2019-12-21 00:43:41 -0500

    update projects to use latest version; TODO pei example is not parsing with pyxtuml

--- examples/GPS/gui.sh
+++ examples/GPS/gui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CLASSPATH=~/.m2/repository/io/ciera/io.ciera.runtime/1.1.1/io.ciera.runtime-1.1.1.jar:~/.m2/repository/org/json/json/20180813/json-20180813.jar:~/.m2/repository/io/ciera/GPS_Watch/1.0.0-SNAPSHOT/GPS_Watch-1.0.0-SNAPSHOT.jar
+CLASSPATH=~/.m2/repository/io/ciera/io.ciera.runtime/1.1.9/io.ciera.runtime-1.1.9.jar:~/.m2/repository/org/json/json/20180813/json-20180813.jar:~/.m2/repository/io/ciera/GPS_Watch/1.0.0-SNAPSHOT/GPS_Watch-1.0.0-SNAPSHOT.jar
 java -cp $CLASSPATH gui.WatchGui &> /dev/null &
--- examples/MicrowaveOven/gen/run.sh
+++ examples/MicrowaveOven/gen/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CLASSPATH=~/.m2/repository/io/ciera/io.ciera.runtime/1.1.1/io.ciera.runtime-1.1.1.jar:~/.m2/repository/io/ciera/MicrowaveOven/1.0.0-SNAPSHOT/MicrowaveOven-1.0.0-SNAPSHOT.jar
+CLASSPATH=~/.m2/repository/io/ciera/io.ciera.runtime/1.1.9/io.ciera.runtime-1.1.9.jar:~/.m2/repository/io/ciera/MicrowaveOven/1.0.0-SNAPSHOT/MicrowaveOven-1.0.0-SNAPSHOT.jar
 java -cp $CLASSPATH microwaveoven.MicrowaveOvenApplication
