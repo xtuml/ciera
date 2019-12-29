f2053a4b06a793a9f2953830fac5b934153f65fb
Levi Starrett <levi@roxsoftware.com>
2019-12-28 20:37:37 -0500

    update GPS Watch to work with version 1.1.11

--- examples/GPS/gui.sh
+++ examples/GPS/gui.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-CLASSPATH=~/.m2/repository/io/ciera/io.ciera.runtime/1.1.9/io.ciera.runtime-1.1.9.jar:~/.m2/repository/org/json/json/20180813/json-20180813.jar:~/.m2/repository/io/ciera/GPS_Watch/1.0.0-SNAPSHOT/GPS_Watch-1.0.0-SNAPSHOT.jar
+CIERA_VERSION=1.1.11
+CLASSPATH=~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/org/json/json/20180813/json-20180813.jar:~/.m2/repository/io/ciera/GPS_Watch/1.0.0-SNAPSHOT/GPS_Watch-1.0.0-SNAPSHOT.jar
 java -cp $CLASSPATH gui.WatchGui &> /dev/null &
