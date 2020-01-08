4d3921e9c59b3d8a8c3baa79b12c81b64165408b
Levi Starrett <levi@roxsoftware.com>
2020-01-08 10:23:37 -0500

    update PEI project to work with v1.1.11

--- examples/pei/gen/run.sh
+++ examples/pei/gen/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-CLASSPATH=~/.m2/repository/io/ciera/io.ciera.runtime/1.1.9/io.ciera.runtime-1.1.9.jar:~/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:~/.m2/repository/io/ciera/pei/1.0.0-SNAPSHOT/pei-1.0.0-SNAPSHOT.jar
-java -cp $CLASSPATH pei.PeiApplication < /dev/null
+CIERA_VERSION=1.1.11
+CLASSPATH=~/.m2/repository/io/ciera/runtime/1.1.11/runtime-1.1.11.jar:~/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:~/.m2/repository/io/ciera/pei/1.0.0-SNAPSHOT/pei-1.0.0-SNAPSHOT.jar
+if [[ $# == 1 ]]; then
+  java -cp $CLASSPATH pei.PeiApplication < $1
+else
+  java -cp $CLASSPATH pei.PeiApplication < /dev/null
+fi
