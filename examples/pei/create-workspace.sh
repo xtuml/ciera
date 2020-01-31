6881c39d0d8238f69fef225a70a407cdff58e000
Levi Starrett <levi@roxsoftware.com>
2020-01-31 01:57:59 -0500

    updated PEI project

--- examples/pei/create-workspace.sh
+++ examples/pei/create-workspace.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+if [[ -z ${BPHOME+x} ]]; then
+  echo "BPHOME environment variable not set"
+  exit 1
+fi
+echo "-------------------------------------"
 echo "Preparing workspace..."
 export WORKSPACE=`mktemp -d`
 $BPHOME/bridgepoint -nosplash -data $WORKSPACE -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll $PWD/../../src/runtime
 $BPHOME/bridgepoint -nosplash -data $WORKSPACE -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll $PWD
+echo "Done."
+echo "-------------------------------------"
 echo "Run this command to set up workspace:"
 echo
-echo "export WORKSPACE=$WORKSPACE"
+echo "  export WORKSPACE=$WORKSPACE"
 echo
+echo "-------------------------------------"
