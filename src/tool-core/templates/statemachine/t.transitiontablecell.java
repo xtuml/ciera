7c786728e72a4140bddd5e1dfec0768e26c0df96
Levi Starrett <levi@roxsoftware.com>
2020-06-11 22:49:15 -0400

    job #11938 fix bug affecting two transition actions between the same two states

--- src/tool-core/templates/statemachine/t.transitiontablecell.java
+++ src/tool-core/templates/statemachine/t.transitiontablecell.java
@@ -5,7 +5,7 @@ IGNORE\
 .else
 (event) -> {\
 .if (has_txn_action)
-${self.state_name}_txn_to_${end_state_name}_action(${parameter_list});\
+${self.state_name}_${evt_name}_txn_to_${end_state_name}_action(${parameter_list});\
 .end if
 .if (has_entry_action)
 ${end_state_name}_entry_action(${parameter_list});\
