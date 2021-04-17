1e0f3e523b0683c8a1190d5cf647636f3c4c4176
amullarney <alasdar@sbcglobal.net>
2021-04-17 10:02:58 -0700

    InterimSave
    LINK uses Rel number, not name. Improved var, attr naming.
    Serializes all binary associations; not associatives - yet.

--- src/tool-sql/templates/loader/t.mlinkserializer.def.java
+++ src/tool-sql/templates/loader/t.mlinkserializer.def.java
@@ -1,7 +1,7 @@
-    public void serialize_${sel_name} ( ${class_name} $l{class_name}_inst, PrintStream out ) throws XtumlException {
-    	${set_type} links = $l{class_name}_inst.${sel_name}();
-    	for ( ${ref_name} link : links ) {
-            out.print( "LINK ${rel_name} ID2(" );
+    public void serialize_${selector_name}( ${class_name} $l{class_name}_inst, PrintStream out ) throws XtumlException {
+    	${set_type_name} links = $l{class_name}_inst.${selector_name}();
+    	for ( ${referred_name} link : links ) {
+            out.print( "LINK ${rnum_str} ID2(" );
             out.print($l{class_name}_inst.getInstanceId().serialize()  );
             out.print(", ");
             out.print(link.getInstanceId().serialize() );
