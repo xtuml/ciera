1d376ba649c03bfbbbcb700cfb5f0e1d16b3e99d
amullarney <alasdar@sbcglobal.net>
2021-04-21 16:25:36 -0700

    SerializerAccessor
    Link serializers navigate to find UniqueId accessor values.
    Work-in-progress; saving proof of concept for now.

--- src/tool-sql/templates/loader/t.slinkserializer.def.java
+++ src/tool-sql/templates/loader/t.slinkserializer.def.java
@@ -1,9 +1,9 @@
     public void serialize_${selector_name}( ${class_name} $l{class_name}_inst, PrintStream out ) throws XtumlException {
     	if ( $l{class_name}_inst.${selector_name}() != null ) {
             out.print( "LINK ${rnum_str} ID2(" );
-            out.print($l{class_name}_inst.getInstanceId().serialize()  );
+            ${form_out}
             out.print(", ");
-            out.print($l{class_name}_inst.${selector_name}().getInstanceId().serialize() );
+            ${part_out}
             out.println( ");" );
     	}
     }
