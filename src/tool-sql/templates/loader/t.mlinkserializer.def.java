980d86774f0a340fe2077e5cd54280632557e8ab
amullarney <alasdar@sbcglobal.net>
2021-04-24 16:41:20 -0700

    Interim
    Adding associative serialization first steps.
    These are initiated from sqlLoader relationships.
    Binary association serialization should move from transformClassInstance

--- src/tool-sql/templates/loader/t.mlinkserializer.def.java
+++ src/tool-sql/templates/loader/t.mlinkserializer.def.java
@@ -1,7 +1,7 @@
     public void serialize_${selector_name}( ${class_name} $l{class_name}_inst, PrintStream out ) throws XtumlException {
     	${set_type_name} links = $l{class_name}_inst.${selector_name}();
     	for ( ${referred_name} ${iterator}_inst : links ) {
-            out.print( "LINK ${rnum_str} ID2(" );
+            out.print( "LINK2 ${rnum_str} IDS(" );
     ${first_out}
             out.print(", ");
     ${second_out}
--- src/tool-sql/templates/loader/t.slinkserializer.def.java
+++ src/tool-sql/templates/loader/t.slinkserializer.def.java
@@ -1,6 +1,6 @@
     public void serialize_${selector_name}( ${class_name} $l{class_name}_inst, PrintStream out ) throws XtumlException {
     	if ( $l{class_name}_inst.${selector_name}() != null ) {
-            out.print( "LINK ${rnum_str} ID2(" );
+            out.print( "LINK2 ${rnum_str} IDs(" );
     ${first_out}
             out.print(", ");
     ${second_out}
