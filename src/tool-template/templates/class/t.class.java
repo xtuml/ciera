4f1339b20cf676569472a59118f1c1fbcf3d65d5
johnrwolfe <j_r_wolfe@msn.com>
2021-08-11 14:13:34 -0700

    Support shared classes
    Another layer in this onion.

--- src/tool-template/templates/class/t.class.java
+++ src/tool-template/templates/class/t.class.java
@@ -2,7 +2,7 @@ package ${self.package};
 
 ${imports}
 
-public interface ${self.name} extends IModelInstance<${self.name},${self.comp_name}> {
+public interface ${self.name} extends IXtumlType {
 
     // attributes
 ${attributes}
