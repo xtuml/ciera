92fb4bbf99bf7eda39bc6fd733db3d8af5c4d76d
Levi Starrett <levi@roxsoftware.com>
2021-11-17 11:32:11 -0500

    starting to implement loggers and action home interface

---
+++ src/runtime/src/main/java/io/ciera/runtime/summit2/action/InstanceActionHome.java
@@ -0,0 +1,9 @@
+package io.ciera.runtime.summit2.action;
+
+import io.ciera.runtime.summit2.domain.ObjectInstance;
+
+public interface InstanceActionHome extends ActionHome {
+    
+    public ObjectInstance self();
+
+}
---
+++ src/runtime/src/main/java/io/ciera/runtime/summit2/application/ExecutionContext.java
@@ -0,0 +1,5 @@
+package io.ciera.runtime.summit2.application;
+
+public class ExecutionContext {
+
+}
