70de3eccd9a88bb829257242f70b9c3fb3ea38a4
Levi Starrett <levi@roxsoftware.com>
2021-11-24 14:42:39 -0500

    bump MO to 3.0.0

--- examples/MicrowaveOven/run.sh
+++ examples/MicrowaveOven/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CIERA_VERSION=2.6.2
+CIERA_VERSION=3.0.0-SNAPSHOT
 CLASSPATH=$HOME/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$HOME/.m2/repository/io/ciera/MicrowaveOven/1.0.0-SNAPSHOT/MicrowaveOven-1.0.0-SNAPSHOT.jar
 java -version
 java -cp $CLASSPATH microwaveoven.MicrowaveOvenApplication $@
