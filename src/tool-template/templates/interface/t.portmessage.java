8a7525a5a519b73e9b8247f130e65053721412a5
amullarney <alasdar@sbcglobal.net>
2021-11-30 16:50:38 -0800

    Add inst_ref cast to outgoing msg parms.
    Moving on to inst_ref_set.

--- src/tool-template/templates/interface/t.portmessage.java
+++ src/tool-template/templates/interface/t.portmessage.java
@@ -1,9 +1,9 @@
     public void ${self.msg_name}(${parameter_list}) throws XtumlException {
-.if ( inbound )
 ${parmcasts}\
+.if ( inbound )
 ${body}
 .else
         if ( satisfied() ) send(new ${self.iface_name}.$c{self.msg_name}(${invocation_parameter_list}));
-        else ${body}\
+        else { ${body}\
     }
 .end if
