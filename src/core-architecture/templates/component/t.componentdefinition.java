a1ccb884c1b86ad65cee786c9b35e2967816d0f0
Levi Starrett <levi@roxsoftware.com>
2021-12-01 00:08:19 -0500

    update component code gen

--- src/core-architecture/templates/component/t.componentdefinition.java
+++ src/core-architecture/templates/component/t.componentdefinition.java
@@ -2,20 +2,14 @@ package ${self.package};
 
 ${imports}
 
-public class ${self.name} extends ${self.extends} {
-
-    private Map<String, Class<?>> classDirectory;
+public class ${self.name} extends Domain {
 
     // utilities
 ${utilities}
 
-    public ${self.name}(IApplication app, IRunContext runContext, int populationId) {
-        super(app, runContext, populationId);
-${instance_extent_initializers}
-${relationship_extent_initializers}
+    public ${self.name}(String name, Application application) {
+        super(name, application);
 ${utility_initializers}
-        classDirectory = new TreeMap<>();
-${class_directory}
     }
 
     // domain functions
@@ -27,63 +21,18 @@ ${relationship_modifiers}
     // instance selections
 ${instance_selectors}
 
-    // relationship selections
-${relationship_selectors}
-
     // ports
 ${ports}
 
     // component initialization function
     @Override
-    public void initialize() throws XtumlException {
+    public void initialize() {
 ${init}
     }
 
-.if ( "" != self.version )
     @Override
-    public String getVersion() {
-        Properties prop = new Properties();
-        try {
-            prop.load(getClass().getResourceAsStream("${self.name}Properties.properties"));
-        } catch (IOException e) { /* do nothing */ }
-        return prop.getProperty("version", "Unknown");
-    }
-.end if
-.if ( "" != self.version_date )
-    @Override
-    public String getVersionDate() {
-        Properties prop = new Properties();
-        try {
-            prop.load(getClass().getResourceAsStream("${self.name}Properties.properties"));
-        } catch (IOException e) { /* do nothing */ }
-        return prop.getProperty("version_date", "Unknown");
-    }
-.end if
-
-    @Override
-    public boolean addInstance( IModelInstance<?,?> instance ) throws XtumlException {
-        if ( null == instance ) throw new BadArgumentException( "Null instance passed." );
-        if ( instance.isEmpty() ) throw new EmptyInstanceException( "Cannot add empty instance to population." );
-${instance_adds}
-        return false;
-    }
-
-    @Override
-    public boolean removeInstance( IModelInstance<?,?> instance ) throws XtumlException {
-        if ( null == instance ) throw new BadArgumentException( "Null instance passed." );
-        if ( instance.isEmpty() ) throw new EmptyInstanceException( "Cannot remove empty instance from population." );
-${instance_removes}
-        return false;
-    }
-
-    @Override
-    public ${self.name} context() {
-        return this;
-    }
-
-    @Override
-    public Class<?> getClassByKeyLetters(String keyLetters) {
-        return classDirectory.get(keyLetters);
+    public ${self.name} getDomain() {
+        return (${self.name}) super.getDomain();
     }
 
 }
--- src/core-architecture/templates/component/t.instancepopulationselector.java
+++ src/core-architecture/templates/component/t.instancepopulationselector.java
@@ -1,6 +1,3 @@
-    private ${set_name} ${self.class_name}_extent;
-    public ${set_name} ${self.name}() {
-        ${set_name} ${self.name} = new ${set_name}Impl();
-        ${self.name}.addAll(${self.class_name}_extent);
-        return ${self.name};
+    public ${self.class_name}.Set ${self.name}() {
+        return new ${self.class_name}.Set(getAllInstances(${self.class_name}.class));
     }
