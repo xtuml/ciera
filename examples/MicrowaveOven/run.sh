0522e1f45c856f422986ef91a0a98e9c06c096dc
Levi Starrett <levi@roxsoftware.com>
2021-12-24 22:16:27 -0500

    update MO

--- examples/MicrowaveOven/run.sh
+++ examples/MicrowaveOven/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CIERA_VERSION=3.0.0-SNAPSHOT
 CLASSPATH=$HOME/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$HOME/.m2/repository/io/ciera/MicrowaveOven/2.0.0-SNAPSHOT/MicrowaveOven-2.0.0-SNAPSHOT.jar
+PROPS=-Dio.ciera.runtime.logLevel="FINEST"
 java -version
-java -cp $CLASSPATH microwaveoven.MicrowaveOvenApplication $@
+java $PROPS -cp $CLASSPATH microwaveoven.MicrowaveOvenApplication $@
