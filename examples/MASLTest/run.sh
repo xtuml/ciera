5f44b217d7ca3136f35ec76ed275bea5d8797094
Levi Starrett <levi@roxsoftware.com>
2021-12-28 22:38:32 -0500

    update test model for timers

---
+++ examples/MASLTest/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CIERA_VERSION=3.0.0-SNAPSHOT
+CLASSPATH=$HOME/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$HOME/.m2/repository/org/xtuml/MASLTest/1.0.0-SNAPSHOT/MASLTest-1.0.0-SNAPSHOT.jar
+PROPS=-Dio.ciera.runtime.logLevel=FINEST
+java $PROPS -cp $CLASSPATH testapplication.TestApplication $@
