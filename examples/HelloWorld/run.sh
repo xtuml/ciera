9b182345b4d84c038517a93a04708da729dbc72a
Levi Starrett <levi@roxsoftware.com>
2021-12-29 16:31:39 -0500

    update HelloWorld to v3

--- examples/HelloWorld/run.sh
+++ examples/HelloWorld/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CIERA_VERSION=2.6.3-SNAPSHOT
+CIERA_VERSION=3.0.0-SNAPSHOT
 CLASSPATH=$HOME/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$HOME/.m2/repository/io/ciera/HelloWorld/1.0.0-SNAPSHOT/HelloWorld-1.0.0-SNAPSHOT.jar
 java -cp $CLASSPATH helloworldapplication.HelloWorldApplication $@
