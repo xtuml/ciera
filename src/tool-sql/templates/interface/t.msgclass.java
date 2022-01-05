1db719f03b4bd3fa9570fc3392beb565d43934bb
amullarney <alasdar@sbcglobal.net>
2022-01-05 08:03:57 -0800

    Name all message classes
    Add name to from-client messages

--- src/tool-sql/templates/interface/t.msgclass.java
+++ src/tool-sql/templates/interface/t.msgclass.java
@@ -1,24 +1,19 @@
 package ${pkg};
 
   public class ${self.name}Msg {
-.if ( not self.to_provider )
     private String messageName;
-.end if;
 ${data_block}
     public ${self.name}Msg() {
     }
-.if ( parmlist != "" )
-    public ${self.name}Msg( ${parmlist} ) {
+    public ${self.name}Msg( String messageName${parmlist} ) {
+      this.messageName = messageName;
 ${parm_block}\
     }
-.end if
 ${code_block}\
-.if ( not self.to_provider )
     public void setMessageName( String messageName ) {
 	  this.messageName = messageName;
     }
     public String getMessageName() {
 	  return messageName;
     }
-.end if
 }
