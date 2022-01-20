8ebfb0226deb4997413376b65ed029481872fdb0
Levi Starrett <levi@roxsoftware.com>
2022-01-19 22:14:58 -0500

    update example projects to reference utility project

--- examples/MicrowaveOven/run.sh
+++ examples/MicrowaveOven/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CIERA_VERSION=3.0.0-SNAPSHOT
-CLASSPATH=$HOME/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$HOME/.m2/repository/io/ciera/MicrowaveOven/2.0.0-SNAPSHOT/MicrowaveOven-2.0.0-SNAPSHOT.jar
+CLASSPATH=$HOME/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$HOME/.m2/repository/io/ciera/runtime-util/$CIERA_VERSION/runtime-util-$CIERA_VERSION.jar:$HOME/.m2/repository/io/ciera/MicrowaveOven/2.0.0-SNAPSHOT/MicrowaveOven-2.0.0-SNAPSHOT.jar
 PROPS=-Dio.ciera.runtime.logLevel="FINEST"
 java -version
 java $PROPS -cp $CLASSPATH microwaveoven.MicrowaveOvenApplication $@
