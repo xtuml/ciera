1e07eba3aa625326ff8b3fa8c77ed33e992f27f2
Levi Starrett <levi@roxsoftware.com>
2022-02-03 14:46:48 -0500

    Fix issue with relate/unrelate using
    The link instance expression is evaluated twice. This is actually not an
    issue at all with OAL since the link instance expression must be a
    simple variable reference, however MASL allows the link instnace
    expression to be any expression of the correct instance type. Because of
    this, an expression which has side effects can be used (e.g. create
    instance expression or service invocation). Obviously such an expression
    cannot be allowed to be evaluated twice, so this change makes it so that
    it is evaluated once and stored in a local variable which is passed to
    the relate and unrelate calls.

--- src/core-architecture/templates/statement/t.relate.java
+++ src/core-architecture/templates/statement/t.relate.java
@@ -1,1 +1,9 @@
+.// only evaluate the expression once
+.if (not_empty next_coupled_relate)
+final ${form_expression.type_reference} _link${self.statement_number} = ${form_expr};
+getDomain().relate_${self.rel_name}(_link${self.statement_number}, ${part_expr});
+.elif (not_empty prev_coupled_relate)
+getDomain().relate_${self.rel_name}(_link${prev_coupled_relate.statement_number}, ${part_expr});
+.else
 getDomain().relate_${self.rel_name}(${form_expr}, ${part_expr});
+.end if
--- src/core-architecture/templates/statement/t.unrelate.java
+++ src/core-architecture/templates/statement/t.unrelate.java
@@ -1,1 +1,9 @@
+.// only evaluate the expression once
+.if (not_empty next_coupled_unrelate)
+final ${form_expression.type_reference} _link${self.statement_number} = ${form_expr};
+getDomain().unrelate_${self.rel_name}(_link${self.statement_number}, ${part_expr});
+.elif (not_empty prev_coupled_unrelate)
+getDomain().unrelate_${self.rel_name}(_link${prev_coupled_unrelate.statement_number}, ${part_expr});
+.else
 getDomain().unrelate_${self.rel_name}(${form_expr}, ${part_expr});
+.end if
