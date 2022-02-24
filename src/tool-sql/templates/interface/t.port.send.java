872515ca647e1844e3a732a0beeef1aebe28496e
amullarney <alasdar@sbcglobal.net>
2022-02-23 22:48:46 -0800

    WebSocket topic change
    Eliminate slash suffix

--- src/tool-sql/templates/interface/t.port.send.java
+++ src/tool-sql/templates/interface/t.port.send.java
@@ -3,11 +3,11 @@
         String msgname = message.getName();
         String payload = (String)message.getParms().toString();
         SpringMsg springmsg = new SpringMsg ( msgname, payload );
-        String topic = "/topic/${topic}/";
+        String topic = "/topic/${topic}";
 .if ( add_sub_topic != "" )
         JSONObject jobj = new JSONObject(payload);
         String subtopic = jobj.getString("${add_sub_topic}");
-        topic = topic + subtopic;
+        topic = topic + "/" + subtopic;
 .end if 
         this.template.convertAndSend( topic, springmsg );
     }
