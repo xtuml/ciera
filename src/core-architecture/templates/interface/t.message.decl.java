54fb54f4278610260734e2e8cd8aedc57fdf524d
Levi Starrett <levi@roxsoftware.com>
2022-02-26 22:28:55 -0500

    add generated serial version ID to messages and events

--- src/core-architecture/templates/interface/t.message.decl.java
+++ src/core-architecture/templates/interface/t.message.decl.java
@@ -1,6 +1,10 @@
 public static class $c{self.name} extends PortMessage implements Message {
+
+    private static final long serialVersionUID = ${self.serial_version_id}L;
+
     public $c{self.name}(${parameter_list}) {
         super($_u{self.name}, Map.<String, Object>of(${invocation_parameter_list}));
     }
+
 }
 
