9f34f301d9eef440ecfd38b67f9b5e1d60e9504e
Levi Starrett <levi@roxsoftware.com>
2022-04-08 11:36:27 -0400

    job #12431 add MASL utility classes

---
+++ src/runtime-masl-util/src/main/java/org/xtuml/masl/util/Logger.java
@@ -0,0 +1,121 @@
+package org.xtuml.masl.util;
+
+public class Logger {
+
+    public Logger() {
+    }
+
+    public Logger(Object domain) {
+        this();
+    }
+
+    public static enum Priority {
+        Fatal, Critical, Error, Warning, Notice, Information, Debug, Trace;
+    }
+
+    public void log(final Priority priority, final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void log(final Priority priority, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void trace(final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void trace(final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void debug(final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void debug(final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void information(final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void information(final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void notice(final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void notice(final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void warning(final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void warning(final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void error(final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void error(final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void critical(final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void critical(final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void fatal(final String logger, final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void fatal(final String message) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void printLoggers() {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void setLogLevel(final String logger, final Priority priority) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void setLogLevel(final Priority priority) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/runtime-masl-util/src/main/java/org/xtuml/masl/util/Strings.java
@@ -0,0 +1,411 @@
+package org.xtuml.masl.util;
+
+import io.ciera.runtime.api.domain.Variable;
+
+import java.util.List;
+import java.util.Set;
+
+public class Strings {
+
+    public Strings() {
+    }
+
+    public Strings(Object domain) {
+        this();
+    }
+
+    public boolean contains(final String input, final String substring) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Byte> decodeBase64(final String base64Encoded) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String encodeBase64(final List<Byte> rawData) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean ends_with(final String input, final String suffix) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_all(final String input, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_all(final Variable<String> target, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_first(final String input, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_first(final Variable<String> target, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_head(final String input, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_head(final Variable<String> target, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_last(final String input, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_last(final Variable<String> target, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_next(final String input, final String search, final int start_pos) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_next(final Variable<String> target, final String search, final int start_pos) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_nth(final String input, final String search, final int n) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_nth(final Variable<String> target, final String search, final int n) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_substring(final String input, final int start_pos) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_substring(final String input, final int start_pos, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_substring(final Variable<String> target, final int start_pos) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_substring(final Variable<String> target, final int start_pos, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String erase_tail(final String input, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void erase_tail(final Variable<String> target, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String head(final String input, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String insert(final String input, final int position, final String insertion) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void insert(final Variable<String> target, final int position, final String insertion) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String join(final List<String> strings, final String separator) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<List<String>> parse_csv(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_all(final String input, final String search, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_all(final Variable<String> target, final String search, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_first(final String input, final String search, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_first(final Variable<String> target, final String search, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_head(final String input, final int length, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_head(final Variable<String> target, final int length, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_last(final String input, final String search, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_last(final Variable<String> target, final String search, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_next(final String input, final String search, final int start_pos,
+            final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_next(final Variable<String> target, final String search, final int start_pos,
+            final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_nth(final String input, final String search, final int n, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_nth(final Variable<String> target, final String search, final int n,
+            final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_substring(final String input, final int start_pos, final int length,
+            final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_substring(final String input, final int start_pos, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_substring(final Variable<String> target, final int start_pos, final int length,
+            final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_substring(final Variable<String> target, final int start_pos, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String replace_tail(final String input, final int length, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void replace_tail(final Variable<String> target, final int length, final String replacement) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Integer> search_all(final String input, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public int search_first(final String input, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public int search_last(final String input, final String search) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public int search_next(final String input, final String search, final int start_pos) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public int search_nth(final String input, final String search, final int n) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> split_lines(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String squeeze(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void squeeze(final Variable<String> target) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean starts_with(final String input, final String prefix) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String substring(final String input, final int start_pos) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String substring(final String input, final int start_pos, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String tail(final String input, final int length) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String to_lower_case(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void to_lower_case(final Variable<String> target) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String to_upper_case(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void to_upper_case(final Variable<String> target) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize(final String input, final Set<Character> dropped_separators) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize(final String input, final Set<Character> dropped_separators,
+            final Set<Character> retained_separators) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize_fixed_size(final String input, final List<Integer> sizes, final boolean wrap,
+            final boolean allow_partial) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize_keep_empty(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize_keep_empty(final String input, final Set<Character> dropped_separators) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize_keep_empty(final String input, final Set<Character> dropped_separators,
+            final Set<Character> retained_separators) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize_quoted(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize_quoted(final String input, final Set<Character> separators) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> tokenize_quoted(final String input, final Set<Character> separators,
+            final Set<Character> quotes, final Set<Character> escapes) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String trim(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void trim(final Variable<String> target) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String trim_leading(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void trim_leading(final Variable<String> target) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public String trim_trailing(final String input) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+    public void trim_trailing(final Variable<String> target) {
+        // TODO Insert your implementation here
+        throw new UnsupportedOperationException();
+    }
+
+}
