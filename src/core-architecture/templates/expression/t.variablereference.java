ed797602c92d3fadf563ade68b7f662a26636c1a
Levi Starrett <levi@roxsoftware.com>
2022-04-09 00:39:53 -0400

    job #12431 fix bug with passing by-ref vars by value

--- src/core-architecture/templates/expression/t.variablereference.java
+++ src/core-architecture/templates/expression/t.variablereference.java
@@ -2,7 +2,7 @@
 _final${finalization_num}_\
 .end if
 ${var_name}\
-.if (is_passed_by_ref and (empty act_parm) and (empty assign_lhs) and (not self.declaration))
+.if (is_passed_by_ref and (empty by_ref_act_parm) and (empty assign_lhs) and (not self.declaration))
 ..get()\
 .end if
 .if (is_passed_by_ref and self.declaration and (empty assign_lhs))
