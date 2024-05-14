486f915826dbc648bf85b0403e5a09ad2e0d843f
Levi Starrett <levi@roxsoftware.com>
2024-05-14 14:04:02 -0400

    job #12818: Update and add marks related to port implementations

--- src/core-architecture/templates/interface/t.portaccessor.java
+++ src/core-architecture/templates/interface/t.portaccessor.java
@@ -1,5 +1,5 @@
     private ${self.name} ${self.name};
     public ${self.name} ${self.port_name}() {
-        if ( null == ${self.name} ) ${self.name} = new ${self.name}( this, null );
+        if ( null == ${self.name} ) ${self.name} = new ${self.impl_class}( this, null );
         return ${self.name};
     }
