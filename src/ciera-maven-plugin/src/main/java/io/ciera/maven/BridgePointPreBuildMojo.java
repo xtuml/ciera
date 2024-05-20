bf11a7bd641a7a7fec805a9f49407560380836dd
Levi Starrett <levi@roxsoftware.com>
2024-05-20 13:18:43 -0400

    job #12820: Back-patch prebuilder upgrades from v3 dev

--- src/ciera-maven-plugin/src/main/java/io/ciera/maven/BridgePointPreBuildMojo.java
+++ src/ciera-maven-plugin/src/main/java/io/ciera/maven/BridgePointPreBuildMojo.java
@@ -1,78 +1,131 @@
 package io.ciera.maven;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.eclipse.aether.repository.LocalArtifactResult;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import net.lingala.zip4j.ZipFile;
 
-import java.lang.ProcessBuilder.Redirect;
-
-/**
- * Goal which runs the BridgePoint pre-builder.
- */
-@Mojo(name="bridgepoint-pre-build", defaultPhase=LifecyclePhase.GENERATE_SOURCES)
+/** Goal which runs the BridgePoint pre-builder. */
+@Mojo(name = "bridgepoint-pre-build", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class BridgePointPreBuildMojo extends AbstractPreBuildMojo {
 
-    private static final String BIN_DIR = "tools/mc/bin";
+  @Parameter(defaultValue = "")
+  private String workspace;
 
-    @Parameter(defaultValue="")
-    private String workspace;
+  @Parameter(defaultValue = "")
+  private String bpHome;
 
-    @Parameter(defaultValue="")
-    private String bpHome;
-
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        if (requiresBuild()) {
-            final String workspace = null == this.workspace || "".equals(this.workspace) ? System.getenv("WORKSPACE") : this.workspace;
-            final String bpHome = null == this.bpHome || "".equals(this.bpHome) ? System.getenv("BPHOME") : this.bpHome;
-            final String cliExe = bpHome + File.separator + "bridgepoint";
-            if (workspace == null || "".equals(workspace.trim()) || bpHome == null || "".equals(bpHome.trim())) {
-                if (bpHome == null || "".equals(bpHome.trim())) {
-                    getLog().error("BPHOME is unset.");
-                }
-                if (workspace == null || "".equals(workspace.trim())) {
-                    getLog().error("WORKSPACE is unset.");
-                }
-                throw new MojoFailureException("Could not execute pre-build.");
-            }
-            else {
-                ProcessBuilder pb = new ProcessBuilder(cliExe, "--launcher.suppressErrors", "-clean", "-noSplash", "-data", workspace, "-application", "org.xtuml.bp.cli.Build", "-project", projectName, "-prebuildOnly").redirectOutput(Redirect.PIPE).redirectError(Redirect.PIPE);
-                getLog().info("Performing BridgePoint pre-build (workspace location: " + workspace + ")...");
-                getLog().info("");
-                printCommand(pb);
-                try {
-                    long startTime = System.currentTimeMillis();
-                    Process proc = pb.start();
-                    Scanner sc = new Scanner(proc.getInputStream());
-                    while (sc.hasNextLine()) {
-                        getLog().info(sc.nextLine());
-                    }
-                    sc.close();
-                    sc = new Scanner(proc.getErrorStream());
-                    while (sc.hasNextLine()) {
-                        getLog().error(sc.nextLine());
-                    }
-                    sc.close();
-                    int duration = (int)(System.currentTimeMillis() - startTime);
-                    int mins = duration / 60000;
-                    int secs = (duration % 60000) / 1000;
-                    int msecs = duration % 1000;
-                    getLog().info("");
-                    getLog().info(String.format("Pre-build duration: %d:%d.%03d", mins, secs, msecs));
-                } catch (IOException e) {
-                    getLog().error("Problem executing pre-builder:", e);
-                }
-            }
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    if (requiresBuild()) {
+      String workspace =
+          null == this.workspace || "".equals(this.workspace)
+              ? System.getenv("WORKSPACE")
+              : this.workspace;
+      final String bpHome =
+          null == this.bpHome || "".equals(this.bpHome) ? System.getenv("BPHOME") : this.bpHome;
+      final String cliExe = bpHome + File.separator + "bridgepoint";
+      if (bpHome == null || "".equals(bpHome.trim())) {
+        getLog().error("BPHOME is unset.");
+        throw new MojoFailureException("Could not execute pre-build.");
+      } else {
+        if (workspace == null || "".equals(workspace.trim())) {
+          getLog().info("WORKSPACE is unset. Creating workspace...");
+          try {
+            workspace = createWorkspace(Path.of(bpHome, "bridgepoint")).toString();
+          } catch (IOException e) {
+            throw new MojoFailureException("Failed to create workspace.", e);
+          }
         }
-        else {
-            getLog().info("Pre-build output up to date.");
+        ProcessBuilder pb =
+            new ProcessBuilder(cliExe, "--launcher.suppressErrors", "-clean", "-noSplash", "-data", workspace, "-application", "org.xtuml.bp.cli.Build", "-project", projectName, "-prebuildOnly")
+                .redirectOutput(Redirect.PIPE)
+                .redirectError(Redirect.PIPE);
+        getLog()
+            .info("Performing BridgePoint pre-build (workspace location: " + workspace + ")...");
+        runProcess("BridgePoint pre-build", pb);
+        try {
+          Files.createDirectories(Path.of(outputFile).getParent());
+          Files.copy(
+              Path.of(
+                  project.getBasedir().getPath(), "gen", "code_generation", projectName + ".sql"),
+              Path.of(outputFile),
+              StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+          throw new MojoFailureException("Could not copy pre-build output to build directory", e);
         }
+      }
+    } else {
+      getLog().info("Pre-build output up to date.");
+    }
+  }
+
+  private Path createWorkspace(final Path bpExe) throws IOException, MojoFailureException {
+    // create a temporary directory
+    final Path workspacePath = Files.createTempDirectory(null);
+
+    final List<LocalArtifactResult> dependencies = getDependencyModels();
+    if (!dependencies.isEmpty()) {
+
+      final Path dependenciesPath = Files.createTempDirectory(null);
+      getLog().info("Unzipping dependenecies to temporary location: " + dependenciesPath);
+
+      // unzip all dependency models into the newly created workspace
+      for (LocalArtifactResult dependencyModel : dependencies) {
+        final Path dependencyPath =
+            Files.createDirectory(
+                dependenciesPath.resolve(
+                    dependencyModel.getRequest().getArtifact().getArtifactId()));
+        final ZipFile zipfile = new ZipFile(dependencyModel.getFile());
+        zipfile.extractAll(dependencyPath.toString());
+      }
+
+      // import dependencies into the workspace
+      final ProcessBuilder pb =
+          new ProcessBuilder(
+                  bpExe.toString(),
+                  "--launcher.suppressErrors",
+                  "-nosplash",
+                  "-data",
+                  workspacePath.toString(),
+                  "-application",
+                  "org.eclipse.cdt.managedbuilder.core.headlessbuild",
+                  "-importAll",
+                  dependenciesPath.toUri().toString())
+              .redirectOutput(Redirect.PIPE)
+              .redirectError(Redirect.PIPE);
+      getLog().info("Importing dependencies (workspace location: " + workspacePath + ")...");
+      runProcess("import dependencies", pb);
     }
 
+    // import this project into the workspace
+    final ProcessBuilder pb =
+        new ProcessBuilder(
+                bpExe.toString(),
+                "--launcher.suppressErrors",
+                "-nosplash",
+                "-data",
+                workspacePath.toString(),
+                "-application",
+                "org.eclipse.cdt.managedbuilder.core.headlessbuild",
+                "-import",
+                project.getBasedir().toURI().toString())
+            .redirectOutput(Redirect.PIPE)
+            .redirectError(Redirect.PIPE);
+    getLog().info("Importing project (workspace location: " + workspacePath + ")...");
+    runProcess("import project", pb);
+
+    return workspacePath.toAbsolutePath();
+  }
 }
--- src/ciera-maven-plugin/src/main/java/io/ciera/maven/PyxtumlPreBuildMojo.java
+++ src/ciera-maven-plugin/src/main/java/io/ciera/maven/PyxtumlPreBuildMojo.java
@@ -1,155 +1,89 @@
 package io.ciera.maven;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.repository.ArtifactRepository;
+import java.io.File;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Scanner;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipEntry;
-
-import java.lang.ProcessBuilder.Redirect;
-
-/**
- * Goal which runs the pyxtuml pre-builder.
- */
-@Mojo(name="pyxtuml-pre-build", defaultPhase=LifecyclePhase.GENERATE_SOURCES)
+/** Goal which runs the pyxtuml pre-builder. */
+@Mojo(name = "pyxtuml-pre-build", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class PyxtumlPreBuildMojo extends AbstractPreBuildMojo {
 
-    @Parameter(defaultValue = "${localRepository}", readonly = true, required = true)
-    private ArtifactRepository localRepository;
+  @Parameter private String[] modelDirs;
 
-    @Parameter
-    private String[] modelDirs;
+  @Parameter(defaultValue = "true")
+  private boolean includeLocalModel;
 
-    @Parameter(defaultValue="true")
-    private boolean includeDependencyModels;
+  @Parameter(defaultValue = "python")
+  private String pythonExecutable;
 
-    @Parameter(defaultValue="true")
-    private boolean includeLocalModel;
+  // pyxtuml-prebuild available logging levels
+  // 0: logging.ERROR,
+  // 1: logging.WARNING,
+  // 2: logging.INFO,
+  // 3: logging.DEBUG,
+  // python -v match
+  // None 0
+  // -v 1
+  // -vv 2
+  // -vvv 3
+  @Parameter(defaultValue = "")
+  private String loggingLevel;
 
-    @Parameter(defaultValue="python")
-    private String pythonExecutable;
-
-    // pyxtuml-prebuild available logging levels
-    // 0: logging.ERROR,
-    // 1: logging.WARNING,
-    // 2: logging.INFO,
-    // 3: logging.DEBUG,
-    // python -v match
-    // None 0
-    // -v 1
-    // -vv 2
-    // -vvv 3
-    @Parameter(defaultValue="")
-    private String loggingLevel;
-
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        String loggingArgument = loggingLevelToVerbosityParam();
-        List<String> resources = new ArrayList<>();
-        if (includeDependencyModels) {
-            resources.addAll(getDependencyModels());
-        }
-        if (includeLocalModel) {
-            resources.add(new File(project.getBasedir(), "models").getPath());
-        }
-        if (null != modelDirs) {
-            resources.addAll(Arrays.asList(modelDirs));
-        }
-        if (requiresBuild()) {
-            new File(project.getBuild().getDirectory()).mkdirs();
-            List<String> cmd = new ArrayList<>();
-            cmd.add(pythonExecutable);
-            cmd.add("-m");
-            cmd.add("bridgepoint.prebuild");
-            cmd.add("-o");
-            cmd.add(outputFile);
-            cmd.addAll(resources);
-            if(loggingArgument != "") {
-                cmd.add(loggingArgument);
-            }
-            ProcessBuilder pb = new ProcessBuilder(cmd).redirectOutput(Redirect.PIPE).redirectError(Redirect.PIPE);
-            getLog().info("Performing pyxtuml pre-build...");
-            printCommand(pb);
-            try {
-                long startTime = System.currentTimeMillis();
-                Process proc = pb.start();
-                Scanner sc = new Scanner(proc.getInputStream());
-                while (sc.hasNextLine()) {
-                    getLog().info(sc.nextLine());
-                }
-                sc.close();
-                sc = new Scanner(proc.getErrorStream());
-                while (sc.hasNextLine()) {
-                    getLog().error(sc.nextLine());
-                }
-                sc.close();
-                if (proc.waitFor() == 0) {
-                    int duration = (int)(System.currentTimeMillis() - startTime);
-                    int mins = duration / 60000;
-                    int secs = (duration % 60000) / 1000;
-                    int msecs = duration % 1000;
-                    getLog().info(String.format("Pre-build duration: %d:%d.%03d", mins, secs, msecs));
-                }
-                else {
-                    getLog().error(String.format("pyxtuml exited with code %d", proc.exitValue()));
-                    throw new MojoFailureException(String.format("pyxtuml exited with code %d", proc.exitValue()));
-                }
-            } catch (IOException | InterruptedException e) {
-                getLog().error("Problem executing pre-builder:", e);
-                throw new MojoFailureException("Problem executing pre-builder", e);
-            }
-        }
-        else {
-            getLog().info("Pre-build output up to date.");
-        }
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    String loggingArgument = loggingLevelToVerbosityParam();
+    List<String> resources = new ArrayList<>();
+    if (includeDependencyModels) {
+      resources.addAll(
+          getDependencyModels().stream()
+              .map(artifact -> artifact.getFile().toString())
+              .collect(Collectors.toList()));
     }
-
-    private String loggingLevelToVerbosityParam() {
-        switch (loggingLevel == null ? "" : loggingLevel) {
-            case "WARN":
-                return "-v";
-            case "INFO":
-                return "-vv";
-            case "DEBUG":
-                return "-vvv";
-            default:
-                return "";
-        }
+    if (includeLocalModel) {
+      resources.add(new File(project.getBasedir(), "models").getPath());
     }
-
-    private List<String> getDependencyModels() {
-        List<String> dependencyModels = new ArrayList<>();
-        for (Artifact artifact : project.getDependencyArtifacts()) {
-            Path artifactPath = Paths.get(localRepository.getBasedir(), localRepository.pathOf(artifact));
-            ZipFile zipfile = null;
-            try {
-                zipfile = new ZipFile(artifactPath.toFile());
-            } catch (IOException e) { /* do nothing */ }
-            if (null != zipfile) {
-                Enumeration<? extends ZipEntry> entries = zipfile.entries();
-                while (entries.hasMoreElements()) {
-                    ZipEntry entry = entries.nextElement();
-                    if (entry.getName().endsWith(".xtuml")) {
-                        dependencyModels.add(artifactPath.toString());
-                        break;
-                    }
-                }
-            }
-        }
-        return dependencyModels;
+    if (null != modelDirs) {
+      resources.addAll(Arrays.asList(modelDirs));
     }
+    if (requiresBuild()) {
+      new File(project.getBuild().getDirectory()).mkdirs();
+      List<String> cmd = new ArrayList<>();
+      cmd.add(pythonExecutable);
+      cmd.add("-m");
+      cmd.add("bridgepoint.prebuild");
+      cmd.add("-o");
+      cmd.add(outputFile);
+      cmd.addAll(resources);
+      if (loggingArgument != "") {
+        cmd.add(loggingArgument);
+      }
+      ProcessBuilder pb =
+          new ProcessBuilder(cmd).redirectOutput(Redirect.PIPE).redirectError(Redirect.PIPE);
+      getLog().info("Performing pyxtuml pre-build...");
+      runProcess("pyxtuml pre-build", pb);
+    } else {
+      getLog().info("Pre-build output up to date.");
+    }
+  }
 
+  private String loggingLevelToVerbosityParam() {
+    switch (loggingLevel == null ? "" : loggingLevel) {
+      case "WARN":
+        return "-v";
+      case "INFO":
+        return "-vv";
+      case "DEBUG":
+        return "-vvv";
+      default:
+        return "";
+    }
+  }
 }
