ec9ea8fbab7cacea2f9e71180291189b84f88e9b
Levi Starrett <levi@roxsoftware.com>
2024-05-30 14:40:08 -0400

    job #12839: remove overhead in relate/unrelate

--- src/core-architecture/templates/relationship/t.relationshipmodifiers.java
+++ src/core-architecture/templates/relationship/t.relationshipmodifiers.java
@@ -1,15 +1,12 @@
     public void relate_${self.name}( ${self.form_name} form, ${self.part_name} part ) throws XtumlException {
         if ( null == form || null == part ) throw new BadArgumentException( "Null instances passed." );
         if ( form.isEmpty() || part.isEmpty() ) throw new EmptyInstanceException( "Cannot relate empty instances." );
-${cardinality_check}        if ( ${self.name}_extent.add( new Relationship( form.getInstanceId(), part.getInstanceId() ) ) ) {
-${relationship_setters}${attribute_propagations}        }
-        else throw new ModelIntegrityException( "Instances could not be related." );
+${cardinality_check}
+${relationship_setters}${attribute_propagations}
     }
 
     public void unrelate_${self.name}( ${self.form_name} form, ${self.part_name} part ) throws XtumlException {
         if ( null == form || null == part ) throw new BadArgumentException( "Null instances passed." );
         if ( form.isEmpty() || part.isEmpty() ) throw new EmptyInstanceException( "Cannot unrelate empty instances." );
-        if ( ${self.name}_extent.remove( ${self.name}_extent.get( form.getInstanceId(), part.getInstanceId() ) ) ) {
-${relationship_unsetters}        }
-        else throw new ModelIntegrityException( "Instances could not be unrelated." );
+${relationship_unsetters}
     }
