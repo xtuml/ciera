43c2af6d42e60b81222a180e71a0673882fc2919
Levi Starrett <levi@roxsoftware.com>
2024-10-17 13:33:36 -0400

    Fix issue with deserialize

--- src/core-architecture/templates/class/t.attribute.deserializer.java
+++ src/core-architecture/templates/class/t.attribute.deserializer.java
@@ -1,5 +1,17 @@
 .if ( is_array )
-inst.${setter_name}(ArrayUtil.deserialize(json.getString("${attribute.base_name}"), ${type_name}::deserialize));
+inst.${setter_name}(ArrayUtil.deserialize(json.getString("${attribute.base_name}"), \
+.if ( is_core )
+${type_name}::deserialize\
+.else
+el -> {
+    try {
+        return ${type_name}.deserialize(el);
+    } catch (XtumlException e) {
+        return null;
+    }
+}\
+.end if
+));
 .else
 inst.${setter_name}(${type_name}.deserialize(json.getString("${attribute.base_name}")));
 .end if
